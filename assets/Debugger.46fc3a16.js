eb76527a536d02c04b9ba59a3ce492618de31243
vojty <vojty@users.noreply.github.com>
2022-10-02 19:23:13 +0000

    deploy: 824d3d90aa96dbdfb4272e01166c77061fdb8550

--- assets/Debugger.46fc3a16.js
+++ assets/Debugger.46fc3a16.js
@@ -1,4 +1,4 @@
-import{s as r,W as n,r as s,a as l,F as i,j as e,_ as c}from"./index.e8529e31.js";import{r as u}from"./romsList.5b5dfa84.js";const o="debugger",d=r.canvas`
+import{s as r,W as n,r as s,a as l,F as i,j as e,_ as c}from"./index.c7fe5a8d.js";import{r as u}from"./romsList.5b5dfa84.js";const o="debugger",d=r.canvas`
   margin-right: auto;
   margin-left: auto;
   display: block;
@@ -23,4 +23,4 @@ import{s as r,W as n,r as s,a as l,F as i,j as e,_ as c}from"./index.e8529e31.js
       margin: 0 !important;
       padding: 0 !important;
   }
-`;function f(){return s.exports.useEffect(()=>{let a=null;return(async()=>{try{a=(await c(()=>import("./debugger_web.483f480a.js"),["assets/debugger_web.483f480a.js","assets/__vite-plugin-wasm-helper.61b72c6d.js"])).start(o,u)}catch(t){console.error(t)}})(),()=>{a?.stop_web(),a?.free()}},[]),l(i,{children:[e(p,{}),e(d,{id:o})]})}export{f as Debugger,f as default};
+`;function f(){return s.exports.useEffect(()=>{let a=null;return(async()=>{try{a=(await c(()=>import("./debugger_web.13bf58c2.js"),["assets/debugger_web.13bf58c2.js","assets/__vite-plugin-wasm-helper.61b72c6d.js"])).start(o,u)}catch(t){console.error(t)}})(),()=>{a?.stop_web(),a?.free()}},[]),l(i,{children:[e(p,{}),e(d,{id:o})]})}export{f as Debugger,f as default};
